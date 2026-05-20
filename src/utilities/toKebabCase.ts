52b4af1605861e6103d93679d20290bf82c08159
rogelioromo <rogelioromo25@gmail.com>
2026-05-20 13:36:16 -0700

    fixed slug creation

--- src/utilities/toKebabCase.ts
+++ src/utilities/toKebabCase.ts
@@ -1,5 +1,10 @@
 export const toKebabCase = (string: string): string =>
   string
-    ?.replace(/([a-z])([A-Z])/g, '$1-$2')
+    ?.normalize('NFD')
+    .replace(/[̀-ͯ]/g, '')
+    .replace(/([a-z])([A-Z])/g, '$1-$2')
+    .replace(/[^a-zA-Z0-9\s-]/g, '')
     .replace(/\s+/g, '-')
+    .replace(/-+/g, '-')
+    .replace(/^-|-$/g, '')
     .toLowerCase()
